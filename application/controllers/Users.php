37f613c9c93620a84c7db722beefc00d65ffb72a
Sigit <noprisigit@gmail.com>
2020-03-19 15:32:56 +0700

    handle update data diri

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -68,14 +68,61 @@ class Users extends CI_Controller {
     }
 
     public function update_data_diri() {
-        $header['title'] = 'Users';
-        $header['subtitle'] = 'Data Diri';
+        $this->form_validation->set_rules('nip_pegawai', 'NIP Pegawai', 'trim|required', ['required' => 'NIP Pegawai Harus Diisi']);
+		$this->form_validation->set_rules('nama_pegawai', 'Nama Pegawai', 'trim|required', ['required' => 'Nama Pegawai Harus Diisi']);
+		$this->form_validation->set_rules('tmpt_lahir_pegawai', 'Tempat Lahir Pegawai', 'trim|required', ['required' => 'Tempat Lahir Pegawai Harus Diisi']);
+		$this->form_validation->set_rules('tgl_lahir_pegawai', 'Tanggal Lahir Pegawai', 'trim|required', ['required' => 'Tanggal Lahir Pegawai Harus Dipilih']);
+		$this->form_validation->set_rules('jns_kelamin_pegawai', 'Jenis Kelamin Pegawai', 'trim|required', ['required' => 'Jenis Kelamin Pegawai Harus Dipilih']);
+		$this->form_validation->set_rules('status_pernikahan_pegawai', 'Status Pernikahan Pegawai', 'trim|required', ['required' => 'Status Pernikahan Pegawai Harus Dipilih']);
+		$this->form_validation->set_rules('no_hp_pegawai', 'No Handphone Pegawai', 'trim|required', ['required' => 'No Handphone Pegawai Harus Diisi']);
+		$this->form_validation->set_rules('jabatan_pegawai', 'Jabatan Pegawai', 'trim|required', ['required' => 'Jabatan Pegawai Tidak Harus Dipilih']);
+		$this->form_validation->set_rules('email_pegawai', 'Email Pegawai', 'trim|required', ['required' => 'Email Pegawai Harus Diisi']);
+		$this->form_validation->set_rules('tgl_masuk_pegawai', 'Tanggal Masuk Pegawai', 'trim|required', ['required' => 'Tanggal Masuk Pegawai Harus Dipilih']);
+		$this->form_validation->set_rules('status_pegawai', 'Status Pegawai', 'trim|required', ['required' => 'Status Pegawai Harus Dipilih']);
+		$this->form_validation->set_rules('agama_pegawai', 'Agama Pegawai', 'trim|required', ['required' => 'Agama Pegawai Harus Dipilih']);
+		$this->form_validation->set_rules('pend_terakhir_pegawai', 'Pendidikan Terakhir Pegawai', 'trim|required', ['required' => 'Pendidikan Terakhir Harus Dipilih']);
+		$this->form_validation->set_rules('alamat_pegawai', 'Alamat Pegawai', 'trim|required', ['required' => 'Alamat Pegawai Harus Diisi']);
 
-        $content['pegawai'] = $this->UserModel->get_data_diri($this->session->userdata('id_user'));
-        // dd($content['pegawai']);
+        if ($this->form_validation->run() == false) {
+            $header['title'] = 'Users';
+            $header['subtitle'] = 'Data Diri';
+    
+            $content['pegawai'] = $this->UserModel->get_data_diri($this->session->userdata('id_user'));
+            $content['jabatan'] = $this->db->get('jabatan')->result_array();
+            // dd($content['pegawai']);
+    
+            $this->load->view('_template/header', $header);
+            $this->load->view('users/update-data-diri', $content);
+            $this->load->view('_template/footer');
+        } else {
+            $image = $_FILES['foto_pegawai']['name'];
 
-        $this->load->view('_template/header', $header);
-		$this->load->view('users/update-data-diri', $content);
-		$this->load->view('_template/footer');
+            if ($image) {
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size'] = 2048;
+                $config['upload_path'] = './assets/dist/img/profile/';
+
+                $this->load->library('upload', $config);
+
+                $data = $this->db->get_where('pegawai', ['id_user' => $this->session->userdata('id_user')])->row_array();
+                // dd($data);
+                if($this->upload->do_upload('foto_pegawai')){
+                    $old_image = $data['foto_pegawai'];
+                    if($old_image != "default.png"){
+                        unlink(FCPATH . 'assets/dist/img/profile/' . $old_image);
+                    }
+                    $image = $this->upload->data('file_name');
+                    $this->db->set('foto_pegawai', $image);
+                }else{
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->db->where('id_pegawai', $this->input->post('id_pegawai'));
+            $this->db->update('pegawai');
+
+            $this->session->set_flashdata('message', 'Diperbaharui');
+			redirect('users/data-diri');
+        }
     }
 }
